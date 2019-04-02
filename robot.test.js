ad2d4f6d6feff5f953b784fd2dc7455bbcfb63c4
Ben Lardie <benlardie@gmail.com>
2019-04-02 14:16:08 -0400

    todos test done

--- robot.test.js
+++ robot.test.js
@@ -11,44 +11,61 @@ test('test_that_foreign_robot_needing_repairs_sent_to_station_1', () => {
   expect(result).toEqual(expectedResult);
 });
 
-test.skip('test_that_vintage_robot_needing_repairs_sent_to_station_2', () => {
+test('test_that_vintage_robot_needing_repairs_sent_to_station_2', () => {
   // arrange
-
+  const vintageRobot = newRobot(true, false, true );
+  const expectedResult = 2;
   // act
-
+  const result = station(vintageRobot);
   // assert
+  expect(result).toEqual(expectedResult);
 });
 
-test.skip('test_that_standard_robot_needing_repairs_sent_to_station_3', () => {
+test('test_that_standard_robot_needing_repairs_sent_to_station_3', () => {
   // arrange
-
+  const regularRobot = newRobot(true, false, false);
+  const expectedResult = 3;
   // act
-
+  const result = station(regularRobot);
   // assert
+  expect(result).toEqual(expectedResult);
 });
 
-test.skip('test_that_robot_in_good_condition_sent_to_station_4', () => {
+test('test_that_robot_in_good_condition_sent_to_station_4', () => {
   // arrange
-
+  const goodRobot = newRobot(false, false, false);
+  const expectedResult = 4;
   // act
-
+  const result = station(goodRobot);
   // assert
+  expect(result).toEqual(expectedResult);
 });
 
-test.skip('test_prioritize_tasks_with_empty_todo_list_returns_negative_one', () => {
+test('test_prioritize_tasks_with_empty_todo_list_returns_negative_one', () => {
   // arrange
+  const todoRobot = newRobot(true, true, false, []);
+  const expectedResult = -1;
 
   // act
+  const result = prioritizeTasks(todoRobot);
 
   // assert
-})
+  expect(result).toEqual(expectedResult);
+});
 
-test.skip('test_prioritize_tasks_with_todos_returns_max_todo_value', () => {
+test('test_prioritize_tasks_with_todos_returns_max_todo_value', () => {
   // arrange
+  const lotsTodoRobot = newRobot(true, true, false);
+  const expectedResult = 3;
+  lotsTodoRobot.todos.push(1);
+  lotsTodoRobot.todos.push(2);
+  lotsTodoRobot.todos.push(3);
 
   // act
+  const result = prioritizeTasks(lotsTodoRobot);
 
   // assert
+  expect(result).toEqual(expectedResult);
 });
 
 test.skip('test_workday_on_day_off_returns_false', () => {
